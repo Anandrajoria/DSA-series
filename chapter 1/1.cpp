d8a30fc816c1d3e8ba8f45df16539055d44d629f
Anandrajoriya <anandrajoriya03@gmail.com>
2025-08-18 22:57:18 +0530

    file

---
+++ chapter 1/1.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"namestey dunia";
+    return 0;
+}
---
+++ chapter 1/dataType_and_variable.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=12;
+    cout<<a<<endl;
+
+    char ch='a';
+    cout<<ch<<endl;
+
+    float f=1.2;
+    cout<<f<<endl;
+
+    bool bl=true;
+    cout<<bl<<endl;
+
+    double d=1.23;
+    cout<<d<<endl;
+
+    int size=sizeof(d);
+    cout<<"size of d is :" <<size<<endl;
+
+    return 0;
+}
